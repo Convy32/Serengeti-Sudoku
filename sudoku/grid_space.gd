f66153e82b1c4bd06238e96d44244637f1771f17
Conor Brodie <103478268+Convy32@users.noreply.github.com>
2026-05-26 23:16:21 +1200

    Better display values - 26/05
    Update display values to have a larger size, better font, and centered
    Working towards a checkrow function, button and initial script added

--- sudoku/grid_space.gd
+++ sudoku/grid_space.gd
@@ -3,6 +3,7 @@ extends ColorRect
 
 var label 
 var active = false
+var box_value
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +12,11 @@ func _ready() -> void:
 	color = Color.WHITE
 	label = Label.new()
 	add_child(label)
-	label.self_modulate = Color.BLACK
+	label.size = Vector2(50, 50)
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	
+
 	
 
 
@@ -30,9 +35,10 @@ func _gui_input(event: InputEvent) -> void:
 func _input(event: InputEvent) -> void:
 	
 		if event is InputEventKey and event.pressed and active:
-			if OS.get_keycode_string(event.keycode) in "0123456789":
+			if OS.get_keycode_string(event.keycode) in "123456789":
 				var inputnum = OS.get_keycode_string(event.keycode)
 				label.text = inputnum
+				box_value = inputnum
 			elif OS.get_keycode_string(event.keycode) == "Backspace":
 				label.text = ""
 			
