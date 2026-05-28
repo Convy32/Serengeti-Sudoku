3e60aa4037ef2f46592104331a1b31bf2527d65d
brodiec23019-whs <brodie.c.23019@gmail.com>
2026-05-28 15:01:59 +1200

    Add check all rows and columns functions
    Functions to check all rows and functions, next to work on is individual rows the the coordinate system
    Plus a few buttons that will be organised into the menu bar

---
+++ sudoku/globals.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var all_boxes_complete = false
--- sudoku/main.gd
+++ sudoku/main.gd
@@ -23,13 +23,48 @@ func check_spaces(spaces: Array):
 
 
 func _on_check_boxes_pressed() -> void:
+	Globals.all_boxes_complete = false
 	var complete = 0
-	for i in range(2, 11):
-		if check_spaces($GridContainer.get_child(i).get_children()) == true:
-			complete += 1
+	
+	for box in $GridContainer.get_children():
+		if box is GridContainer:
+			if check_spaces(box.get_children()) == false:  #change to true
+					complete += 1
+	
 	if complete == 9:
-		print("all complete")
+		Globals.all_boxes_complete = true
+		print(Globals.all_boxes_complete)
+
+
+func check_current_row():
+	pass
 
 
 func _on_check_current_box_pressed() -> void:
 	check_spaces($GridContainer.get_child(2).get_children())
+
+
+func check_all_rows() -> void:
+	for r1 in range(3):
+		for r2 in range(3):
+			var row = []
+			for i in range(3):
+				for j in range(3):
+					print(i+r1*3, " ", j+r2*3)
+					row.append($GridContainer.get_child(i+r1*3+2).get_child(j+r2*3))
+				
+			print()
+			print(check_spaces(row))
+
+
+func check_all_columns() -> void:
+	for r1 in range(3):
+		for r2 in range(3):
+			var column = []
+			for i in range(3):
+				for j in range(3):
+					print(i+r1*3, " ", j+r2*3)
+					column.append($GridContainer.get_child(j+r2*3+2).get_child(i+r1*3))
+				
+			print()
+			print(check_spaces(column))
